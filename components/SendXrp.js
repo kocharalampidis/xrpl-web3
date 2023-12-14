c19dd3d9ee210e5e233e569c3bdeca039fa1fcaf
kocharalampidis <kocharalampidis@gmail.com>
2023-12-14 15:39:56 +0200

    refactoring

--- components/SendXrp.js
+++ components/SendXrp.js
@@ -3,8 +3,14 @@ import { useEffect, useState } from "react";
 import xrpl, { Client, Wallet, xrpToDrops, dropsToXrp } from "xrpl";
 
 const SendXrp = () => {
-  // Testnet
-  const client = new Client("wss://s.altnet.rippletest.net:51233");
+  // Testnet wss://s.altnet.rippletest.net:51233
+  const testnet = "wss://s.altnet.rippletest.net:51233";
+
+  const [client, setClient] = useState(new Client(testnet));
+  const [senderWallet, setSenderWallet] = useState();
+  // Wallet.fromSecret("sEd7UqeGUxspVocmTQszsoCe3rqEven")
+  const [recieverWallet, setRecieverWallet] = useState();
+  //Wallet.fromSecret("sEdST9Jg1XPfXKDkg1kdDvvNpyF5u9U")
 
   const connectToTestnet = async () => {
     console.log("Connecting to Testnet...");
@@ -27,53 +33,49 @@ const SendXrp = () => {
   };
 
   const createWalletFromSeed = async () => {
-    //await client.connect();
-    if (client.isConnected) {
-      console.log("client connected:", client.isConnected());
-    }
+    console.log("Creating Sender and Destination wallets");
 
     // Create wallet on testnet from seed - Working with XRP Faucets
     const wallet1 = Wallet.fromSecret("sEd7UqeGUxspVocmTQszsoCe3rqEven"); // address: rnZaJB553ctyHy4CpYNUzcK8GXCs6SmtUa
     const wallet2 = Wallet.fromSecret("sEdST9Jg1XPfXKDkg1kdDvvNpyF5u9U"); // address: rBiX1FKUx1XScmMESj74aataykTnCzWiST
+
+    setSenderWallet(wallet1);
+    setRecieverWallet(wallet2);
 
     console.log(`wallet1 address: ${wallet1.classicAddress}`);
     console.log(`wallet2 address: ${wallet2.classicAddress}`);
   };
 
   const initPayment = async () => {
-    // Connect to Tesnet
-    await client.connect();
-    if (client.isConnected) {
-      console.log("client connected:", client.isConnected());
-    }
-
     // creating wallets as prerequisite
-    const { wallet: wallet1 } = await client.fundWallet();
+    //const { wallet: wallet1 } = await client.fundWallet();
     // client.fundWallet(null, {
     //   usageContext: "test",
     // });
-    const { wallet: wallet2 } = await client.fundWallet();
+    //const { wallet: wallet2 } = await client.fundWallet();
     // fundWallet(null, {
     //   usageContext: "test",
     // });
 
     console.log("Balances of wallets before Payment tx");
     console.log(
-      await client.getXrpBalance(wallet1.classicAddress),
-      await client.getXrpBalance(wallet2.classicAddress)
+      await client.getXrpBalance(senderWallet.classicAddress),
+      await client.getXrpBalance(recieverWallet.classicAddress)
     );
 
     // create a Payment tx and submit and wait for tx to be validated
     const payment = {
       TransactionType: "Payment",
-      Account: wallet1.classicAddress,
+      Account: senderWallet.classicAddress,
       Amount: "1000",
-      Destination: address,
+      Destination: recieverWallet.classicAddress,
     };
+
+    console.log(payment);
 
     // Submit payment
     const paymentResponse = await client.submitAndWait(payment, {
-      wallet: wallet1,
+      wallet: senderWallet,
     });
     console.log("\nTransaction was submitted. \n");
 
@@ -95,8 +97,8 @@ const SendXrp = () => {
 
     console.log("Balances of wallets after Payment tx:");
     console.log(
-      await client.getXrpBalance(wallet1.classicAddress),
-      await client.getXrpBalance(wallet2.classicAddress)
+      await client.getXrpBalance(senderWallet.classicAddress),
+      await client.getXrpBalance(recieverWallet.classicAddress)
     );
 
     await client.disconnect();
